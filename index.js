8fe67400ea0dd3270995de3da44bbf3778fe14c3
cstylianou98 <cstylianou98@gmail.com>
2023-09-13 17:16:11 +0300

    Fixed bug in backend

--- index.js
+++ index.js
@@ -1,47 +1,3 @@
-// const http = require("http") //http built into node
-
-// const server = http.createServer((req, res) => {
-//     // res.statusCode = 404; // browser will display this error now
-//     console.log(req.url)
-//     console.log(req.method)
-//     res.setHeader("Content-Type", "text/html") // now telling that we have the following content being html
-//     res.end("<img src='https://pbs.twimg.com/media/Dj8XlmjV4AEzsvr.jpg'>") // Creates server but wont show if we don't tell server its HTML
-// }) //req = request obj, res = response obj
-
-// server.listen(3000, ()=> {
-//     console.log("Server Ready")
-// })
-
-//libraries such as express js exist to help. Most widely used htp server for node etc.. This is to give some idea for a server.
-
-
-
-// NOW WE WILL USE EXPRESS PACKAGE in an easy example
-
-// const express = require("express")
-// const app = express()
-// const port = 3000
-
-// app.get("/", (req,res)=>{
-//     res.send("Hello World!")
-// }) // "/" represents home page
-
-// app.get("/chicken", (req,res) => {
-//     res.send("Hello Chicken")
-// })
-
-// app.get("/chicken/:name", (req,res) => {
-//     res.send(req.params) // params will give you the parameter. So in website u go /chicken/bob for example
-//     // res.send(req.query) //will be empty because we dont have a query in /chicken/bob -> would have to be /chicken/bob?apikey=1234. Particularly when you are checking APIs.
-// })
-
-// app.get("/example", (req,res) => {
-//     res.sendStatus (418) //sends status directly to browser. Good practice to do so.
-//     // res.statusCode = 404
-//     res.send()
-// })
-
-
 // FRUIT API
 
 require('dotenv').config()
@@ -51,12 +7,8 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT
 
-// app.use((req,res,next) =>{
-//     console.log("I am a piece of Middleware")
-//     next() //without this, webpage runs forever.
-// })
 
-app.use(cors) //to avoid cors error
+app.use(cors()) //to avoid cors error
 app.use(express.json())
 
 app.get("/", (req,res)=> {
@@ -121,3 +73,4 @@ app.get("/fruits/:name", (req,res)=> {
 app.listen(port, ()=>{
     console.log(`Server is now listening on port ${port}`)
 })
+
